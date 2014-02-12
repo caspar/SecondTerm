69d770f471586c1487e09b5d03928a2a9c926e06
caspar <casparlant@gmail.com>
2014-02-11 20:35:49 -0500

    School commit - Caspar

---
+++ Recursion/HailStone.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class HailStone{
+    int n = 0;
+    
+    public static void main(String[] args){
+	HailStone HS = new HailStone();
+	n = Integer.parseInt(args[1]);
+    }
+
+    public static int hailLen(){
+	
+    }
+
+    public static int hail(){
+	//return 1;
+    }
+    
+    public static int longestHail(){
+	
+    }
+    
+}
--- Recursion/Recursion.java
+++ Recursion/Recursion.java
@@ -7,9 +7,11 @@ public class Recursion{
 	Recursion fact;
 	fact = new Recursion(test);
     }
+    
     public Recursion(int input){
 	System.out.println("Factorial: "+factorial(input));
 	System.out.println("Fibonacci: "+fib(input));
+	System.out.println("Fibonacci Fast: "+fibIter(input));
     }
 
     private int fib(int input){
@@ -20,11 +22,25 @@ public class Recursion{
 	else
 	    return fib(input-1)+fib(input-2);
     }
+
     private int factorial(int input){
        	if (input == 0)
 	    return 1;
 	else{
 	    return input*(factorial(input-1));
 	}
+    }
+    
+    private int fibHelp(int penult, int last, int n){
+	if (n == 0)
+	    return penult; 
+	else 
+	    return fibHelp(last, penult+last, n-1);
+    }
+    private int fibIter(int n){
+	if (n < 2)
+	    return n;
+	else
+	    return fibHelp(0,1,n-2);
     }
 }
