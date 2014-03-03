effc1a0a7b6447d4d17cdbd4db012e0da2172950
caspar <casparlant@gmail.com>
2014-03-03 15:29:04 -0500

    Added msort() - Caspar

--- Sorts/MergeSort.java
+++ Sorts/MergeSort.java
@@ -5,13 +5,36 @@ public class MergeSort{
     
     public static void main(String[] args){
 	try {
-	MergeSort MS = new MergeSort(args[0]);
+	    MergeSort MS = new MergeSort(args[0]);
 	}
 	catch (Exception oops){
 	}
-}
+    }
     
-    
+
+    public void msort(int [] a){
+	//
+	int[] ans;
+	
+	if (a.length > 1){
+	    // make 2 smaller arrays
+	    int[] L1 = new int[] (a.length/2);
+	    int[] L2 = new int[] (a.length/2);
+	    
+	    //copy a to the smaller arrays
+	    arraycopy(a, 0, L1, 0, a.length/2);
+	    arraycopy(a, a.length/2, L2, 0, a.length/2);
+	    
+	    //msort each half
+	    msort(L1);
+	    msort(L2);
+	    
+	    ans = merge(L1,L2);
+	    //copy ans back into a
+	    arraycopy(ans, 0, a, 0, a.length);
+	}
+    }
+   
     public int[] merge(int[] a, int[] b){
 	//pre: both a & b are sorted
 	//post: returned array has all the values of a & b sorted
@@ -24,10 +47,7 @@ public class MergeSort{
 	    else{
 		result[n] = b[n];
 	    }
-	}
-        
-	
+	}      	
     } 
-
     
 }
