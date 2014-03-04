c87e5be77e4fed979f9a058052a9794a9941a15d
caspar <casparlant@gmail.com>
2014-03-04 15:20:11 -0500

    Sorta push - Caspar

--- Sorts/Sorts.java
+++ Sorts/Sorts.java
@@ -3,6 +3,8 @@ import java.util.*;
 
 public class MergeSort{
     
+    public boolean isSorted = false;
+    
     public static void main(String[] args){
 	try {
 	    MergeSort MS = new MergeSort(args[0]);
@@ -10,32 +12,48 @@ public class MergeSort{
 	catch (Exception oops){
 	}
     }
-    
+       
+    private int[] merge(int[] a, int[] b){
+	int[]out=new int[a.length+b.length];
+	int ia=0,ib=0;
+	while (ia<a.length || ib<b.length){
+	    if (ia<a.length && (ib>=b.length || a[ia]<b[ib])){
+		out[ia+ib]=b[ib];
+		ia++;
+	    }else{
+		out[ia+ib]=a[ia];
+		ib++;
+	    }
+	}
+	return out;
+    }
 
-    public void msort(int [] a){
-	//
+    public void mergesort(int [] a){
+	
 	int[] ans;
 	
 	if (a.length > 1){
 	    // make 2 smaller arrays
 	    int[] L1 = new int[] (a.length/2);
-	    int[] L2 = new int[] (a.length/2);
+	    int[] L2 = new int[] ((a.length+1)/2);
 	    
 	    //copy a to the smaller arrays
-	    arraycopy(a, 0, L1, 0, a.length/2);
-	    arraycopy(a, a.length/2, L2, 0, a.length/2);
+	    arraycopy(a, 0,         L1, 0, L1.length);
+	    arraycopy(a, L1.length, L2, 0, L2.length);
 	    
 	    //msort each half
-	    msort(L1);
-	    msort(L2);
+	    mergesort(L1);
+	    mergesort(L2);
 	    
+	    //merge arrays
 	    ans = merge(L1,L2);
+
 	    //copy ans back into a
 	    arraycopy(ans, 0, a, 0, a.length);
 	}
     }
    
-    public int[] merge(int[] a, int[] b){
+    public int[] merge1(int[] a, int[] b){
 	//pre: both a & b are sorted
 	//post: returned array has all the values of a & b sorted
 	int[] result = new int[a.length + b.length];
