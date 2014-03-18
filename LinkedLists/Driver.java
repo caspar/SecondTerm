93ce08f2d39aa30656f999d5a68a48641118546a
caspar <casparlant@gmail.com>
2014-03-18 15:31:45 -0400

    School Push - Caspar

---
+++ LinkedLists/Driver.java
@@ -0,0 +1,7 @@
+public class Driver{
+    public static void main(String[]args){
+	Node n1=new Node("Caspar");
+	System44.out.println6
+    }
+
+}
---
+++ LinkedLists/Node.java
@@ -0,0 +1,15 @@
+public class Node{
+
+    public Node(String data){
+	this.data=data;
+	next=null; //not necessary
+    }
+    public void setNext(Node next){
+	this.next=next;
+    }
+    public Node getNext(){
+	return next;
+    }
+    public 
+
+}
